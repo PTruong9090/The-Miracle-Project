02c02f1dbe751cb01f5f722be21a07f116fa09ce
Phuc Truong <ptruong9090@gmail.com>
2024-05-19 00:02:55 -0700

    Created header

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import logo from '../assets/miracle-project-logo.png'
+import './header.css';
+import './nav-buttons.css';
+
 
 function Header(){
-    return(
-        <>
-        </>
+    return (
+        <div className='header-container'>
+            <header>
+                <div className='logo-container'>
+                    <img src={logo} alt='Logo' className='logo' />
+                </div>
+                <nav>
+                    <Link to='/upload' className="nav-link">Upload</Link>
+                </nav>
+            </header>
+        </div>
     );
 }
 export default Header
